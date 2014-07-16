8eb4df8225e7b0977d5ea63d1ccc4096da041ec9
Frank Kany <frankkany@gmail.com>
2014-07-16 15:37:42 -0500

    Spike specs for enrollment.

---
+++ spec/kairos/enroll_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Kairos::Client do
+
+  context "when working with the Enroll endpoint" do
+    before do
+      @client = Kairos::Client.new(:app_id => "consumerid", :api_key => "consumersecret")
+    end
+
+    describe "'Enroll'" do
+      context "with image url" do
+        it "should return a json response with completed enrollment information" do
+          pending
+        end
+      end
+    end
+  end
+
+end
